8b404b13cf83a4b83843929312f6cd606a642cc0
Waldir Borba Junior <wborbajr@gmail.com>
2020-12-03 15:51:29 -0300

    chore: initial import

--- config/config.go
+++ config/config.go
@@ -6,7 +6,6 @@ import (
 	_ "github.com/nakagami/firebirdsql"
 )
 
-
 func Konnekt() (db *sql.DB, err error) {
 	db, err = sql.Open("firebirdsql", "SYSDBA:masterkey@192.168.0.2/C:/Program Files (x86)/CompuFour/Clipp/Base/CLIPP.FDB?charset=WIN1252")
 
