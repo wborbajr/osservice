06413adb273ecc83c17428869cf4055a82296e17
Waldir Borba Junior <wborbajr@gmail.com>
2020-12-05 17:11:08 -0300

    chore: initial import

--- apis/apidata.go
+++ apis/apidata.go
@@ -1,7 +1,6 @@
 package apis
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -12,15 +11,16 @@ import (
 	_struct "github.com/wborbajr/osservice/struct"
 )
 
+type Ret struct {
+	IsiData []_struct.StructData
+	err error
+}
+
 func GetOS(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	var errcount int
-
-	// IsiData := make(chan []_struct.StructData)
-	var IsiData []_struct.StructData
-	var err2ara, err2cwb, err2lon, err2nat, err2rec error
+	chanret := make(chan Ret)
 
 	params := mux.Vars(r)
 
@@ -32,71 +32,92 @@ func GetOS(w http.ResponseWriter, r *http.Request) {
 	os := params["os"]
 
 	dbara, errara := config.Konnekt_ara()
-	dbcwb, errcwb := config.Konnekt_cwb()
-	dblon, errlon := config.Konnekt_lon()
-	dbnat, errnat := config.Konnekt_nat()
-	dbrec, errrec := config.Konnekt_rec()
+	// dbcwb, errcwb := config.Konnekt_cwb()
+	// dblon, errlon := config.Konnekt_lon()
+	// dbnat, errnat := config.Konnekt_nat()
+	// dbrec, errrec := config.Konnekt_rec()
 
 	if errara == nil {
 		_modelsara := models.ModelGetData{DB:dbara}
-		// go func() {
-			IsiData, err2ara = _modelsara.GetOS(doc, os)
-			if err2ara == sql.ErrNoRows {
-				errcount++
-			}
-		// }()
+		go func() {
+			var r Ret
+			r.IsiData, r.err = _modelsara.GetOS(doc, os)
+			chanret <- r
+		}()
 	}
-	if errcwb == nil {
-		_modelscwb := models.ModelGetData{DB:dbcwb}
-		// go func() {
-			IsiData, err2cwb = _modelscwb.GetOS(doc, os)
-			if err2cwb == sql.ErrNoRows {
-				errcount++
-			}
-		// }()
-	}
-	if errlon == nil {
-		_modelslon := models.ModelGetData{DB:dblon}
-		// go func() {
-			IsiData, err2lon = _modelslon.GetOS(doc, os)
-			if err2lon == sql.ErrNoRows {
-				errcount++
-			}
-		// }()
-	}
-	if errnat == nil {
-		_modelsnat := models.ModelGetData{DB:dbnat}
-		// go func() {
-			IsiData, err2nat = _modelsnat.GetOS(doc, os)
-			if err2nat == sql.ErrNoRows {
-				errcount++
-			}
-		// }()
-	}
-	if errrec == nil {
-		_modelsrec := models.ModelGetData{DB:dbrec}
-		// go func() {
-			IsiData, err2rec = _modelsrec.GetOS(doc, os)
-			if err2rec == sql.ErrNoRows {
-				errcount++
-			}
-		// }()
-	}
+
+	IsiData, _ := <-chanret
+
+	// fmt.Println( <-chanret )
+		fmt.Println(IsiData)
+
+	// if err {
+	// 	fmt.Println("ERR")
+	// 	fmt.Println(IsiData)
+	// } else {
+	// 	fmt.Println("ELSE ERR")
+	// 	fmt.Println( <-chanret )
+
+	// }
+
 
 	var Response _struct.ResponseData
+	Response.Status = http.StatusOK
+	Response.Message = "Sukses"
+	Response.Data = IsiData
+	restponWithJson(w, http.StatusOK, Response)
 
-	// record not found
-	if errcount == 5 {
-		Response.Status = http.StatusInternalServerError
-		Response.Message = "OS Not Found"
-		Response.Data = nil
-		restponWithJson(w, http.StatusInternalServerError, Response)
-	} else {
-		Response.Status = http.StatusOK
-		Response.Message = "Sukses"
-		Response.Data = IsiData
-		restponWithJson(w, http.StatusOK, Response)
-	}
+	// if errcwb == nil {
+	// 	_modelscwb := models.ModelGetData{DB:dbcwb}
+	// 	// go func() {
+	// 		IsiData, err2cwb = _modelscwb.GetOS(doc, os)
+	// 		if err2cwb == sql.ErrNoRows {
+	// 			errcount++
+	// 		}
+	// 	// }()
+	// }
+	// if errlon == nil {
+	// 	_modelslon := models.ModelGetData{DB:dblon}
+	// 	// go func() {
+	// 		IsiData, err2lon = _modelslon.GetOS(doc, os)
+	// 		if err2lon == sql.ErrNoRows {
+	// 			errcount++
+	// 		}
+	// 	// }()
+	// }
+	// if errnat == nil {
+	// 	_modelsnat := models.ModelGetData{DB:dbnat}
+	// 	// go func() {
+	// 		IsiData, err2nat = _modelsnat.GetOS(doc, os)
+	// 		if err2nat == sql.ErrNoRows {
+	// 			errcount++
+	// 		}
+	// 	// }()
+	// }
+	// if errrec == nil {
+	// 	_modelsrec := models.ModelGetData{DB:dbrec}
+	// 	// go func() {
+	// 		IsiData, err2rec = _modelsrec.GetOS(doc, os)
+	// 		if err2rec == sql.ErrNoRows {
+	// 			errcount++
+	// 		}
+	// 	// }()
+	// }
+
+	// var Response _struct.ResponseData
+
+	// // record not found
+	// if errcount == 5 {
+	// 	Response.Status = http.StatusInternalServerError
+	// 	Response.Message = "OS Not Found"
+	// 	Response.Data = nil
+	// 	restponWithJson(w, http.StatusInternalServerError, Response)
+	// } else {
+	// 	Response.Status = http.StatusOK
+	// 	Response.Message = "Sukses"
+	// 	Response.Data = IsiData
+	// 	restponWithJson(w, http.StatusOK, Response)
+	// }
 
 
 
