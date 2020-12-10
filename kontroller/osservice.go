e3136f9394e3ea66c85064c6d66d925e321c45a3
Waldir Borba Junior <wborbajr@gmail.com>
2020-12-10 12:23:40 -0300

    chore: fixing unused variable

--- kontroller/osservice.go
+++ kontroller/osservice.go
@@ -1,9 +1,21 @@
 package kontroller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // GetAllOS - retrieve customer order service
 func GetAllOS(c *fiber.Ctx) error {
+
+	// Parsing parameters
+	paramDoc := c.Params("doc")
+	paramOs := c.Params("os")
+
+	log.Println(paramDoc)
+	log.Println(paramOs)
+
 	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 		"success": false,
 		"message": "OS not found",
