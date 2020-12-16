aa48b7e43d0c15fd7b1cf9c39e603662cb5aa86f
Waldir Borba Junior <wborbajr@gmail.com>
2020-12-16 16:15:43 -0300

    chore: 1st implementation of service order search.

--- kontroller/kontroller.go
+++ kontroller/kontroller.go
@@ -1,7 +1,6 @@
 package kontroller
 
 import (
-	"database/sql"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,8 +13,16 @@ import (
 // https://play.golang.org/p/mWw59cjYPh7
 // https://stackoverflow.com/questions/27795036/create-chan-for-func-with-two-return-args#27795117
 
+var errAra error
+var errCwb error
+var errLon error
+var errNat error
+var errRec error
+
 // GetAllOS - retrieve customer order service
 func GetAllOS(c *fiber.Ctx) error {
+
+	count := 0
 
 	// Parsing parameters
 	paramDoc := c.Params("doc")
@@ -23,39 +30,116 @@ func GetAllOS(c *fiber.Ctx) error {
 
 	serviceOrder := model.ServiceOrder{}
 
-	if err := database.KonnektAra(); err != nil {
-		log.Println(err)
+	if err := database.KonnektAra(); err == nil {
+		errAra = database.DB.QueryRow("SELECT * FROM TB_OS WHERE ID_OS=? AND ID_CLIENTE=?", paramOs, paramDoc).Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus)
+	}
+	if err := database.KonnektCwb(); err == nil {
+		errCwb = database.DB.QueryRow("SELECT * FROM TB_OS WHERE ID_OS=? AND ID_CLIENTE=?", paramOs, paramDoc).Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus)
+	}
+	if err := database.KonnektLon(); err == nil {
+		errLon = database.DB.QueryRow("SELECT * FROM TB_OS WHERE ID_OS=? AND ID_CLIENTE=?", paramOs, paramDoc).Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus)
+	}
+	if err := database.KonnektNat(); err == nil {
+		errNat = database.DB.QueryRow("SELECT * FROM TB_OS WHERE ID_OS=? AND ID_CLIENTE=?", paramOs, paramDoc).Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus)
+	}
+	if err := database.KonnektRec(); err == nil {
+		errRec = database.DB.QueryRow("SELECT * FROM TB_OS WHERE ID_OS=? AND ID_CLIENTE=?", paramOs, paramDoc).Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus)
 	}
 
-	row, err := database.DB.Query("SELECT * FROM TB_OS WHERE ID_OS=$1 AND ID_CLIENTE=$2", paramOs, paramDoc)
+	if errAra == nil {
+		count ++
+		log.Println("ARA Found")
+		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	}else {
+		log.Println("ARA NOT Found")
+	}
+	if errCwb == nil {
+		count ++
+		log.Println("CWB Found")
+		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	}else {
+		log.Println("CWB NOT Found")
+	}
+	if errLon == nil {
+		count ++
+		log.Println("LON Found")
+		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	}else {
+		log.Println("LON NOT Found")
+	}
+	if errNat == nil {
+		count ++
+		log.Println("NAT Found")
+		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	}else {
+		log.Println("NAT NOT Found")
+	}
+	if errRec == nil {
+		count ++
+		log.Println("REC Found")
+		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	}else {
+		log.Println("REC NOT Found")
+	}
 
-	if err != nil {
-		return c.Status(500).JSON(&fiber.Map{
+	if count != 0 {
+		if err := c.JSON(&fiber.Map{
 			"success": false,
-			"message": err,
-		})
-
-	}
-
-	defer row.Close()
-
-	for row.Next() {
-		switch err := row.Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus); err {
-		case sql.ErrNoRows:
-			log.Println("No rows were returned!")
-			return c.Status(500).JSON(&fiber.Map{
+			"message": "Successfully fetched OSService",
+			"osservice": serviceOrder,
+			}); err != nil {
+			c.Status(500).JSON(&fiber.Map{
 				"success": false,
-				"message": err,
-			})
-		case nil:
-			log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
-		default:
-			return c.Status(500).JSON(&fiber.Map{
-				"success": false,
-				"message": err,
-			})
+				"message":  err,
+				})
+			return nil
 		}
+
+	} else {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Service Order not found!",
+			})
+
 	}
+
+
+
+
+	// if errAra != sql.ErrNoRows {
+
+	// }
+
+
+	// row, err := database.DB.Query("SELECT * FROM TB_OS WHERE ID_OS=$1 AND ID_CLIENTE=$2", paramOs, paramDoc)
+
+	// if err != nil {
+	// 	return c.Status(500).JSON(&fiber.Map{
+	// 		"success": false,
+	// 		"message": err,
+	// 	})
+
+	// }
+
+	// defer row.Close()
+
+	// for row.Next() {
+	// 	switch err := row.Scan(&serviceOrder.IdOs, &serviceOrder.IdCliente, &serviceOrder.IdStatus); err {
+	// 	case sql.ErrNoRows:
+	// 		log.Println("No rows were returned!")
+	// 		return c.Status(500).JSON(&fiber.Map{
+	// 			"success": false,
+	// 			"message": err,
+	// 		})
+	// 	case nil:
+	// 		log.Println(serviceOrder.IdOs, serviceOrder.IdCliente, serviceOrder.IdStatus)
+	// 	default:
+	// 		return c.Status(500).JSON(&fiber.Map{
+	// 			"success": false,
+	// 			"message": err,
+	// 		})
+	// 	}
+	// }
 
 	return nil
 }
